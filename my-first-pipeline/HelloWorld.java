f617fa978711328f1868b6acacc62e8686ee91bc
parammohith <86465977+parammohith@users.noreply.github.com>
2025-02-14 16:10:18 +0530

    Update HelloWorld.java

--- my-first-pipeline/HelloWorld.java
+++ my-first-pipeline/HelloWorld.java
@@ -1,5 +1,7 @@
+package com.example;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins and Docker!");
+        System.out.println("Hello, World!");
     }
 }

f8564aab248a0548c29e689a58fda5a63db0bf3d
parammohith <86465977+parammohith@users.noreply.github.com>
2025-09-19 17:03:12 +0530

    Update HelloWorldTest.java

--- my-first-pipeline/HelloWorldTest.java
+++ my-first-pipeline/HelloWorldTest.java
@@ -1,1 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testGreet() {
+        assertEquals("Hello from Maven & Jenkins!", HelloWorld.greet());
+    }
+}
 
